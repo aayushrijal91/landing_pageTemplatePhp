c4a9f8a8f832a279853363eeb311ee63dc34095d
Aayush Rijal <aayush.rijal99@gmail.com>
2022-04-19 11:07:04 +1000

    image render method updated

--- app/functions.php
+++ app/functions.php
@@ -6,3 +6,15 @@ $bcc_email = "";
 $no_reply_email = 'no-reply@icfitouts.com.au';
 $recaptcha_client_secret = '6LeSsm8eAAAAABTDNbqczWmMgvjWjRMmpFPQQnxH';
 $recaptcha_server_secret = '6LeSsm8eAAAAAMOIvIQQ43lCdFz_wswrwZgqNKF2';
+
+function renderImg($filename, $folder)
+{
+    $filename_without_ext = preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename);
+    $src = "./assets/images/" . $folder . "/" . $filename;
+
+    if (file_exists("./assets/images/" . $folder . "/" . $filename_without_ext . ".webp")) {
+        $src = "./assets/images/" . $folder . "/" . $filename_without_ext . ".webp";
+    }
+
+    return "<img src=" . $src . " alt=" . $filename_without_ext . ">";
+}

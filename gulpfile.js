f8983b73fb9c20080110a68d19233ef9c2f287fd
Aayush Rijal <aayush.rijal99@gmail.com>
2022-07-07 10:50:23 +1000

    package json update

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,25 @@
-const gulp = require("gulp"); const path = require('path');
-const sass = require("gulp-sass");
+const { src, dest, watch } = require("gulp");
+const path = require('path');
+const sass = require("gulp-sass")(require('sass'));
 const browserSync = require("browser-sync").create();
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
 
-
-// Compile Scss into Css
 function style() {
-    // 1. Where is my scss file
-    return gulp.src("./scss/**/*.scss")
-        // 2. pass file through sass compiler
+    return src("./scss/**/*.scss")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        // 3. Where do I save the compiled CSS?
-        .pipe(gulp.dest("./app/assets/css"))
-        // 4. Stream changes to all browsers
+        .pipe(dest("./app/assets/css"))
         .pipe(browserSync.stream());
 }
 
 function imgSquash() {
-    return gulp.src("./images/**/*")
+    return src("./images/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("./app/assets/images"));
+        .pipe(dest("./app/assets/images"));
 }
 
 function scripts() {
-    // All JS files to be combined
     const scriptsList = [
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
@@ -34,26 +28,26 @@ function scripts() {
         'scripts/main.js',
 
     ];
-    // Minify the JS and place it in ./assets/js/app.js
-    return gulp.src(scriptsList)
+
+    return src(scriptsList)
         .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./app/assets/js'));
+        .pipe(terser())
+        .pipe(dest('./app/assets/js'));
 }
 
 
-function watch() {
+function watchTask() {
     browserSync.init({
         proxy: encodeURI(`localhost/projects/landing_pages/${path.resolve(__dirname, './').split(path.sep).pop()}/app`),
         injectChanges: true,
     });
-    gulp.watch("./scss/**/*.scss", style);
-    gulp.watch('./scripts/**/*.js', scripts);
-    // gulp.watch("./images/**/*", imgSquash);
-    gulp.watch('./app/**/*.php').on('change', browserSync.reload);
-    gulp.watch("./app/assets/js/**/*.js").on('change', browserSync.reload);
+    watch("./scss/**/*.scss", style);
+    watch('./scripts/**/*.js', scripts);
+    // watch("./images/**/*", imgSquash);
+    watch('./app/**/*.php').on('change', browserSync.reload);
+    watch("./app/assets/js/**/*.js").on('change', browserSync.reload);
 }
 
 exports.style = style;
-exports.watch = watch;
+exports.watch = watchTask;
 
